1d2338d319f5ef755e3d8e0ef9c969a4a8134414
dfam13 <133124260+dfam13@users.noreply.github.com>
2023-05-13 12:07:35 +0200

    update contracts logic

--- InteractiveProtocol.sol
+++ InteractiveProtocol.sol
@@ -49,77 +49,75 @@ contract InteractiveProtocol {
 
         emit TaskCompleted(_taskId, msg.sender, _result);
     }
-}
 
-function getTaskCount() external view returns (uint256) {
-    return taskId;
-}
+    function getTaskCount() external view returns (uint256) {
+        return taskId;
+    }
 
-function getTaskDetails(uint256 _taskId) external view returns (Task memory) {
-    require(_taskId <= taskId, "Invalid task ID");
+    function getTaskDetails(uint256 _taskId) external view returns (Task memory) {
+        require(_taskId <= taskId, "Invalid task ID");
 
-    return tasks[_taskId];
-}
+        return tasks[_taskId];
+    }
 
-function isTaskCompleted(uint256 _taskId) external view returns (bool) {
-    require(_taskId <= taskId, "Invalid task ID");
+    function isTaskCompleted(uint256 _taskId) external view returns (bool) {
+        require(_taskId <= taskId, "Invalid task ID");
 
-    return tasks[_taskId].isCompleted;
-}
+        return tasks[_taskId].isCompleted;
+    }
 
-function getUserTasks() external view returns (uint256[] memory) {
-    uint256[] memory userTaskIds = new uint256[](taskId);
-    uint256 count = 0;
+    function getUserTasks() external view returns (uint256[] memory) {
+        uint256[] memory userTaskIds = new uint256[](taskId);
+        uint256 count = 0;
 
-    for (uint256 i = 1; i <= taskId; i++) {
-        if (tasks[i].user == msg.sender) {
-            userTaskIds[count] = i;
-            count++;
+        for (uint256 i = 1; i <= taskId; i++) {
+            if (tasks[i].user == msg.sender) {
+                userTaskIds[count] = i;
+                count++;
+            }
         }
-    }
 
-    uint256[] memory result = new uint256[](count);
-    for (uint256 i = 0; i < count; i++) {
-        result[i] = userTaskIds[i];
-    }
-
-    return result;
-}
-
-function getNodeTasks() external view returns (uint256[] memory) {
-    uint256[] memory nodeTaskIds = new uint256[](taskId);
-    uint256 count = 0;
-
-    for (uint256 i = 1; i <= taskId; i++) {
-        if (tasks[i].selectedNode == msg.sender) {
-            nodeTaskIds[count] = i;
-            count++;
+        uint256[] memory result = new uint256[](count);
+        for (uint256 i = 0; i < count; i++) {
+            result[i] = userTaskIds[i];
         }
+
+        return result;
     }
 
-    uint256[] memory result = new uint256[](count);
-    for (uint256 i = 0; i < count; i++) {
-        result[i] = nodeTaskIds[i];
+    function getNodeTasks() external view returns (uint256[] memory) {
+        uint256[] memory nodeTaskIds = new uint256[](taskId);
+        uint256 count = 0;
+
+        for (uint256 i = 1; i <= taskId; i++) {
+            if (tasks[i].selectedNode == msg.sender) {
+                nodeTaskIds[count] = i;
+                count++;
+            }
+        }
+
+        uint256[] memory result = new uint256[](count);
+        for (uint256 i = 0; i < count; i++) {
+            result[i] = nodeTaskIds[i];
+        }
+
+        return result;
     }
 
-    return result;
-}
+    function cancelTask(uint256 _taskId) external {
+        Task storage existingTask = tasks[_taskId];
+        require(existingTask.taskId == _taskId, "Invalid task ID");
+        require(existingTask.user == msg.sender, "Only task owner can cancel the task");
+        require(!existingTask.isCompleted, "Task has already been completed");
 
-function cancelTask(uint256 _taskId) external {
-    Task storage existingTask = tasks[_taskId];
-    require(existingTask.taskId == _taskId, "Invalid task ID");
-    require(existingTask.user == msg.sender, "Only task owner can cancel the task");
-    require(!existingTask.isCompleted, "Task has already been completed");
+        delete tasks[_taskId];
+    }
 
-    delete tasks[_taskId];
-}
+    function updateTaskResult(uint256 _taskId, string calldata _result) external {
+        Task storage existingTask = tasks[_taskId];
+        require(existingTask.taskId == _taskId, "Invalid task ID");
+        require(existingTask.selectedNode == msg.sender, "Only selected node can update task result");
+        require(existingTask.isCompleted, "Task has not been completed");
 
-function updateTaskResult(uint256 _taskId, string calldata _result) external {
-    Task storage existingTask = tasks[_taskId];
-    require(existingTask.taskId == _taskId, "Invalid task ID");
-    require(existingTask.selectedNode == msg.sender, "Only selected node can update task result");
-    require(existingTask.isCompleted, "Task has not been completed");
-
-    existingTask.result = _result;
-}
-
+        existingTask.result = _result;
+    }
